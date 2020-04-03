b1958382611d3d063e27a9ab7edef5761c42061f
neethu-ms <msneethu2989@gmail.com>
2020-04-03 22:46:04 +0000

    User authentication

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
