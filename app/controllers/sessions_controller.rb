4d6566eb2393835441293b928f33f0fdbca47350
neethu-ms <msneethu2989@gmail.com>
2020-04-04 02:01:50 +0000

    Modify login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,15 @@
 class SessionsController < ApplicationController
-  def new
+ def new
   end
 
   def create
     user = User.find_by_email(params[:email])
-    # If the user exists AND the password entered is correct.
+    # if the user exists AND the password entered is correct
     if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
-      # logged in when they navigate around our website.
+      # save the user id inside the browser cookie. This is how we keep the user logged in when they navigate around our website.
       session[:user_id] = user.id
       redirect_to '/'
     else
-    # If user's login doesn't work, send them back to the login form.
       redirect_to '/login'
     end
   end
