3528d577798a96e09a36adb3330fc236b5065fe2
neethu-ms <msneethu2989@gmail.com>
2020-04-08 01:23:17 +0000

    Add user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ActiveRecord::Base
   has_secure_password
+  
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { is: 4 }
+  validates :first_name, :last_name, :email, presence: true
+  before_save { self.email.downcase!}
+
+  def self.authenticate_with_password (email, password)
+    if email.strip
+      email = email.strip 
+    end 
+      user = User.find_by(email:email.downcase)
+      puts user
+      if user.authenticate(password)
+        return user
+      end
+      return nil 
+  end 
 end
